de1dace758fdf39e4a4dc09e31e4d1597d4cbdc9
vaibhav sharma <vbsmandi@gmail.com>
2019-04-03 09:11:10 +0530

    Add files via upload

---
+++ AzureClient.ino
@@ -0,0 +1,274 @@
+#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <WiFiUdp.h>
+
+#include <AzureIoTHub.h>
+#include <AzureIoTProtocol_MQTT.h>
+#include <AzureIoTUtility.h>
+
+#define DEVICE_ID "esp32demo1"
+
+#define MESSAGE_MAX_LEN 256
+
+
+static WiFiClientSecure sslClient; // for ESP8266
+
+static int interval = 2000;
+bool temperatureAlert= false;
+const char* ssid     = "Sahil Vabhiav";
+const char* password = "24041990";
+
+// Data upload timer will run for the interval of 20s
+unsigned long msg_Interval = 120000;
+unsigned long msg_Timer = 0;
+
+/*String containing Hostname, Device Id & Device Key in the format:                         */
+/*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
+/*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessSignature=<device_sas_token>"    */
+static const char* connectionString = "HostName=ncdio.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=ezIriOfg84+GDq+2eYAvqKbtJw51UtHMByXOYHtX0iY=";
+
+//Create a char variable to store our JSON format
+const char *messageData = "{\"deviceId\":\"%s\", \"messageId\":%d, \"Random\":%d}";
+
+const char *onSuccess = "\"Successfully invoke device method\"";
+const char *notFound = "\"No method found\"";
+
+static bool messagePending = false;
+static bool messageSending = true;
+
+
+static int messageCount = 0;
+
+/*void blinkLED()
+{
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+}*/
+
+void initWifi()
+{
+    // Attempt to connect to Wifi network:
+    Serial.printf("Attempting to connect to SSID: %s.\r\n", ssid);
+
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        // Get Mac Address and show it.
+        // WiFi.macAddress(mac) save the mac address into a six length array, but the endian may be different. The huzzah board should
+        // start from mac[0] to mac[5], but some other kinds of board run in the oppsite direction.
+        uint8_t mac[6];
+        WiFi.macAddress(mac);
+        Serial.printf("You device with MAC address %02x:%02x:%02x:%02x:%02x:%02x connects to %s failed! Waiting 10 seconds to retry.\r\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5], ssid);
+        delay(5000);
+    }
+    Serial.printf("Connected to wifi %s.\r\n", ssid);
+}
+
+void initTime()
+{
+    time_t epochTime;
+    configTime(0, 0, "pool.ntp.org", "time.nist.gov");
+
+    while (true)
+    {
+        epochTime = time(NULL);
+
+        if (epochTime == 0)
+        {
+            Serial.println("Fetching NTP epoch time failed! Waiting 2 seconds to retry.");
+            delay(2000);
+        }
+        else
+        {
+            Serial.printf("Fetched NTP epoch time is: %lu.\r\n", epochTime);
+            break;
+        }
+    }
+}
+
+static IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle;
+void setup()
+{
+    //pinMode(LED_PIN, OUTPUT);
+
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    Serial.println("Serial successfully inited.");
+    delay(2000);
+    initWifi();
+    initTime();
+
+    /*
+     * AzureIotHub library remove AzureIoTHubClient class in 1.0.34, so we remove the code below to avoid
+     *    compile error
+    */
+
+    // initIoThubClient();
+    iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, MQTT_Protocol);
+    if (iotHubClientHandle == NULL)
+    {
+        Serial.println("Failed on IoTHubClient_CreateFromConnectionString.");
+        delay(2000);
+        while (1);
+    }
+
+    IoTHubClient_LL_SetOption(iotHubClientHandle, "product_info", "ESP8266");
+    IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, receiveMessageCallback, NULL);
+    IoTHubClient_LL_SetDeviceMethodCallback(iotHubClientHandle, deviceMethodCallback, NULL);
+    IoTHubClient_LL_SetDeviceTwinCallback(iotHubClientHandle, twinCallback, NULL);
+}
+
+void loop()
+{
+    if (!messagePending && messageSending)
+    {
+        if(millis()-msg_Timer >= msg_Interval){
+        msg_Timer = millis();
+        int randomNum = random(0,50);
+        char messagePayload[MESSAGE_MAX_LEN];
+        snprintf(messagePayload,MESSAGE_MAX_LEN,messageData,DEVICE_ID,messageCount++,randomNum);
+        sendMessage(iotHubClientHandle, messagePayload, temperatureAlert);
+    }
+    IoTHubClient_LL_DoWork(iotHubClientHandle);
+    delay(10);
+    }
+}
+
+
+static void sendCallback(IOTHUB_CLIENT_CONFIRMATION_RESULT result, void *userContextCallback)
+{
+    if (IOTHUB_CLIENT_CONFIRMATION_OK == result)
+    {
+        Serial.println("Message sent to Azure IoT Hub");
+//        blinkLED();
+    }
+    else
+    {
+        Serial.println("Failed to send message to Azure IoT Hub");
+    }
+    messagePending = false;
+}
+
+static void sendMessage(IOTHUB_CLIENT_LL_HANDLE iotHubClientHandle, char *buffer, bool temperatureAlert)
+{
+    IOTHUB_MESSAGE_HANDLE messageHandle = IoTHubMessage_CreateFromByteArray((const unsigned char *)buffer, strlen(buffer));
+    if (messageHandle == NULL)
+    {
+        Serial.println("Unable to create a new IoTHubMessage.");
+    }
+    else
+    {
+        MAP_HANDLE properties = IoTHubMessage_Properties(messageHandle);
+        Map_Add(properties, "temperatureAlert", temperatureAlert ? "true" : "false");
+        Serial.printf("Sending message: %s.\r\n", buffer);
+        if (IoTHubClient_LL_SendEventAsync(iotHubClientHandle, messageHandle, sendCallback, NULL) != IOTHUB_CLIENT_OK)
+        {
+            Serial.println("Failed to hand over the message to IoTHubClient.");
+        }
+        else
+        {
+            messagePending = true;
+            Serial.println("IoTHubClient accepted the message for delivery.");
+        }
+
+        IoTHubMessage_Destroy(messageHandle);
+    }
+}
+
+
+IOTHUBMESSAGE_DISPOSITION_RESULT receiveMessageCallback(IOTHUB_MESSAGE_HANDLE message, void *userContextCallback)
+{
+    IOTHUBMESSAGE_DISPOSITION_RESULT result;
+    const unsigned char *buffer;
+    size_t size;
+    if (IoTHubMessage_GetByteArray(message, &buffer, &size) != IOTHUB_MESSAGE_OK)
+    {
+        Serial.println("Unable to IoTHubMessage_GetByteArray.");
+        result = IOTHUBMESSAGE_REJECTED;
+    }
+    else
+    {
+        /*buffer is not zero terminated*/
+        char *temp = (char *)malloc(size + 1);
+
+        if (temp == NULL)
+        {
+            return IOTHUBMESSAGE_ABANDONED;
+        }
+
+        strncpy(temp, (const char *)buffer, size);
+        temp[size] = '\0';
+        Serial.printf("Receive C2D message: %s.\r\n", temp);
+        free(temp);
+//        blinkLED();
+    }
+    return IOTHUBMESSAGE_ACCEPTED;
+}
+
+int deviceMethodCallback(
+    const char *methodName,
+    const unsigned char *payload,
+    size_t size,
+    unsigned char **response,
+    size_t *response_size,
+    void *userContextCallback)
+{
+    Serial.printf("Try to invoke method %s.\r\n", methodName);
+    const char *responseMessage = onSuccess;
+    int result = 200;
+
+    if (strcmp(methodName, "start") == 0)
+    {
+      Serial.println("Start sending data.");
+      messageSending = true;
+    }
+    else if (strcmp(methodName, "stop") == 0)
+    {
+      Serial.println("Stop sending data");
+      messageSending = false;
+    }
+    else
+    {
+        Serial.printf("No method %s found.\r\n", methodName);
+        responseMessage = notFound;
+        result = 404;
+    }
+
+    *response_size = strlen(responseMessage);
+    *response = (unsigned char *)malloc(*response_size);
+    strncpy((char *)(*response), responseMessage, *response_size);
+
+    return result;
+}
+
+void twinCallback(DEVICE_TWIN_UPDATE_STATE updateState,const unsigned char *payLoad,size_t size,void *userContextCallback)
+{
+    StaticJsonBuffer<MESSAGE_MAX_LEN> jsonBuffer;
+    char *temp = (char *)malloc(size + 1);
+    for (int i = 0; i < size; i++)
+    {
+        temp[i] = (char)(payLoad[i]);
+    }
+    temp[size] = '\0';
+    JsonObject &root = jsonBuffer.parseObject(temp);
+        if (!root.success())
+    {
+        Serial.printf("Parse %s failed.\r\n", temp);
+        return;
+    }
+
+    if (root["desired"]["interval"].success())
+    {
+        interval = root["desired"]["interval"];
+    }
+    else if (root.containsKey("interval"))
+    {
+        interval = root["interval"];
+    }
+    free(temp);
+}
+
